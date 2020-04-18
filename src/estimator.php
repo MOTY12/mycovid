30de2a10ed46bed088ad8c031fa851be0238ea5f
mukhtar2944 <50269513+mukhtar2944@users.noreply.github.com>
2020-04-18 01:04:14 -0700

    Update estimator.php

--- src/estimator.php
+++ src/estimator.php
@@ -5,25 +5,38 @@ $periodType=$_POST["periodType"];
 $totalHospitalBeds=$_POST["totalHospitalBeds"];
 $Population=$_POST["Population"];
 
-  
+  function days($periodType,$timeToElapse){
+    switch ($periodType){
+      case "weeks";
+        $days= $timeToElapse * 7;
+    break;
+    case "months";
+    $days= $timeToElapse * 30;
+    break;
+    default:
+    $days = $timeToElapse;
+    }
+    return $days;         
+  }
+  $day = days($periodType,$timeToElapse);
   function covid19ImpactEstimator($data)
   { 
      $Impact["currentlyinfected"]=$data['reportedCases'] * 10;
      $severeImpact["currentlyinfected"]=$data["reportedCases"] *50;
      
-    $Impact["infectionsbyrequestedtime"] = $Impact['currentlyinfected'] * 1024 . 30 ;
-    $severeImpact["infectionsbyrequestedtime"] = $severeImpact['currentlyinfected'] * 1024 * 30;
+    $Impact["infectionsbyrequestedtime"] = $Impact['currentlyinfected'] * 1024 . ($day/3) ;
+    $severeImpact["infectionsbyrequestedtime"] = $severeImpact['currentlyinfected'] * 1024 . ($day/3);
 
   $Impact["severeCasesByRequestedTime"]=$Impact["infectionsbyrequestedtime"] * 0.15;
   $severeImpact["severeCasesByRequestedTime"]=$severeImpact["infectionsbyrequestedtime"] * 0.15;
-  $Impact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.35) - $Impact["severeCasesByRequestedTime"]);
-  $severeImpact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.35)-$severeImpact["severeCasesByRequestedTime"]);
-  $Impact["casesForICUByRequestedTime"]=$Impact["infectionsbyrequestedtime"]*0.05;
-  $severeImpact["casesForICUByRequestedTime"]=$severeImpact["infectionsbyrequestedtime"]*0.05;
-  $Impact["casesForVentilatorsByRequestedTime"]=intval($Impact["infectionsbyrequestedtime"]*0.02);
+  $Impact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"] * 0.65) - $Impact["severeCasesByRequestedTime"]);
+  $severeImpact["hospitalBedsByRequestedTime"]=intval(($data["totalHospitalBeds"]*0.65) - $severeImpact["severeCasesByRequestedTime"]);
+  $Impact["casesForICUByRequestedTime"]=$Impact["infectionsbyrequestedtime"] * 0.05;
+  $severeImpact["casesForICUByRequestedTime"]=$severeImpact["infectionsbyrequestedtime"] * 0.05;
+  $Impact["casesForVentilatorsByRequestedTime"]=intval($Impact["infectionsbyrequestedtime"] * 0.02);
   $severeImpact["casesForVentilatorsByRequestedTime"]=intval($severeImpact["infectionsbyrequestedtime"]*0.02);
-  $Impact["dollarsInFlight"] = ($Impact["infectionsbyrequestedtime"] * 0.65) * 1.5 * 30;
-  $severeImpact["dollarsInFlight"] = ($severeImpact["infectionsbyrequestedtime"] * 0.65) * 1.5 * 30 ;
+  $Impact["dollarsInFlight"] = ($Impact["infectionsbyrequestedtime"] * 0.65) * 1.5 . $day;
+  $severeImpact["dollarsInFlight"] = ($severeImpact["infectionsbyrequestedtime"] * 0.65) * 1.5 . $day;
 
   return [
     "data"=> $data,
